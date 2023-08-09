0ba1aadaa95c72d267ac182e974df4ebfca4db1c
Tatyana Araujo <tatyana.lima@sptech.school>
2023-08-09 20:25:56 -0300

    add laço de repetiçao

---
+++ nuneros-randomicos/src/main/java/LacosDeRepeticao.java
@@ -0,0 +1,16 @@
+public class LacosDeRepeticao {
+    public static void main(String[] args) {
+        for (int i=0; i<=11; i++) {
+            System.out.println(i);
+        }
+
+        for (int i = 10; i < 11 ; i--) {
+            System.out.println(i);
+        }
+
+        int i = 0;
+        while(i <= 10) {
+            System.out.println(i);
+        }
+    }
+}
