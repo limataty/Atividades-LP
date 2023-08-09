d63bd441a634d6d9250ea2aece0de8c1641a5742
Tatyana Araujo <tatyana.lima@sptech.school>
2023-08-09 20:35:56 -0300

    att laço de repetição

--- nuneros-randomicos/src/main/java/LacosDeRepeticao.java
+++ nuneros-randomicos/src/main/java/LacosDeRepeticao.java
@@ -11,6 +11,14 @@ public class LacosDeRepeticao {
         int i = 0;
         while(i <= 10) {
             System.out.println(i);
+            i++;
         }
+
+        System.out.println("do while 0 a 10");
+        int j = 0;
+        do {
+            System.out.println(j);
+            j++;
+        }while(j <= 10);
     }
 }
